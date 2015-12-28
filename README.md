c807f2ffd4c54570f76191a8a38c2281cb5e7967
Spencer Hakim <sha@trustpilot.com>
2015-12-27 20:42:53 -0500

    Added an installation script

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # zsh-snarf
-Snarf theme for Oh-My-Zsh
+Snarf theme for [Oh-My-Zsh](http://ohmyz.sh/). Requires the `battery` and `jsontools` plugins.
 
+### Installation
+```bash
+sh -c "$(wget https://raw.github.com/spencerhakim/zsh-snarf/master/install.sh -O -)"
+```
+
+This will not modify any settings; it only simplifies installation. It places the font and .zsh-theme in the appropriate
+directories, and imports the iTerm2 preset. You are still required to switch to these files manually.
+
+### Screenshot
 ![Screenshot](screenshot.png)
 (Snarf theme, with Monokai Snarf iTerm2 colorscheme, and Knack typeface AKA Nerd Font-patched Hack typeface)
---
+++ install.sh
@@ -0,0 +1,24 @@
+#/bin/zsh
+
+echo 'Downloading zsh-snarf files...'
+
+# Install any pre-reqs
+if [[ !$(command -v wget >/dev/null 2>&1) ]]; then
+  brew install wget >/dev/null 2>&1
+fi
+
+# Install zsh theme
+wget 'https://raw.githubusercontent.com/spencerhakim/zsh-snarf/master/snarf.zsh-theme'  \
+    -nv -O "$ZSH_CUSTOM/themes/snarf.zsh-theme"
+
+# Install iTerm2 colorscheme
+COLORSCHEME=$(wget 'https://raw.githubusercontent.com/spencerhakim/zsh-snarf/master/Monokai%20Snarf.itermcolors' -nv -O -)
+defaults write com.googlecode.iterm2 'Custom Color Presets' -dict-add 'Monokai Snarf' "$COLORSCHEME"
+
+# Install font
+wget 'https://github.com/spencerhakim/zsh-snarf/raw/master/Knack%20Regular%20Nerd%20Font%20Complete.otf'  \
+    -nv -O "~/Library/Fonts/Knack Regular Nerd Font Complete.otf"
+
+echo -n 'Finished downloading zsh-snarf files. Remember to edit your ~/.zshrc; you may also need to quit/re-open iTerm2'
+echo -n ' for the color preset and font to appear in their respective dropdown lists.'
+echo
