316f2bd1314f8e315fa5b72f7fbaf02fc22f170a
Spencer Hakim <sha@trustpilot.com>
2015-12-27 21:14:51 -0500

    Handle Linux installs

--- install.sh
+++ install.sh
@@ -6,21 +6,27 @@ echo '************************************************************'
 echo 'Downloading zsh-snarf files...'
 
 # Install any pre-reqs
-if [[ !$(command -v wget >/dev/null 2>&1) ]]; then
-  brew install wget >/dev/null 2>&1
-fi
+[[ ! $(command -v wget) ]] && brew install wget
 
 # Install zsh theme
 wget 'https://raw.githubusercontent.com/spencerhakim/zsh-snarf/master/snarf.zsh-theme'  \
     -nv -O "$ZSH_CUSTOM/themes/snarf.zsh-theme"
 
-# Install iTerm2 colorscheme
-COLORSCHEME=$(wget 'https://raw.githubusercontent.com/spencerhakim/zsh-snarf/master/Monokai%20Snarf.itermcolors' -nv -O -)
-defaults write com.googlecode.iterm2 'Custom Color Presets' -dict-add 'Monokai Snarf' "$COLORSCHEME"
+# Install iTerm2 colorscheme, but only on OS X
+if [[ $(uname) == 'Darwin' ]]; then
+  COLORSCHEME=$(wget 'https://raw.githubusercontent.com/spencerhakim/zsh-snarf/master/Monokai%20Snarf.itermcolors' -nv -O -)
+  defaults write com.googlecode.iterm2 'Custom Color Presets' -dict-add 'Monokai Snarf' "$COLORSCHEME"
+fi
 
 # Install font
+if [[ $(uname) == 'Darwin' ]]; then
+  FONT_DIR="$HOME/Library/Fonts" # OS X
+else
+  FONT_DIR="$HOME/.fonts" # Linux
+fi
+mkdir -p "$FONT_DIR"
 wget 'https://raw.githubusercontent.com/spencerhakim/zsh-snarf/master/Knack%20Regular%20Nerd%20Font%20Complete.otf'  \
-    -nv -O "$HOME/Library/Fonts/Knack Regular Nerd Font Complete.otf"
+    -nv -O "$FONT_DIR/Knack Regular Nerd Font Complete.otf"
 
 echo -n 'Finished downloading zsh-snarf files. Remember to edit your ~/.zshrc; you may also need to quit/re-open iTerm2'
 echo -n ' for the color preset and font to appear in their respective dropdown lists.'
